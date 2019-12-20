3d55c6273c47459f1bc349e02456d6277f7b19e5
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-20 21:41:52 +0530

    Update P02_ArrayRotation.py

--- Arrays/P02_ArrayRotation.py
+++ Arrays/P02_ArrayRotation.py
@@ -1,24 +1,30 @@
 # Author: AKHILESH SANTOSHWAR
 
-from Arrays import Array
-
-def rotation(rotateBy, myArray):
-    for i in range(0, rotateBy):
-        rotateOne(myArray)
-    return myArray
-
-def rotateOne(myArray):
-    for i in range(len(myArray) - 1):
-        myArray[i], myArray[i + 1] = myArray[i + 1], myArray[i]
-
-
-if __name__ == '__main__':
-    myArray = Array(10)
-    for i in range(len(myArray)):
-        myArray.insert(i, i)
-    print('Before Rotation:',myArray)
-    print('After Rotation:',rotation(3, myArray))
+# Python program to cyclically rotate
+#an array by one
+# Method for rotation
+def rotate(A, n):
+   x = A[n - 1]
+   for i in range(n - 1, 0, -1):
+      A[i] = A[i - 1];
+   A[0] = x;
+# Driver function
+A=list()
+n=int(input("Enter the size of the List ::"))
+print("Enter the Element of  List ::")
+for i in range(int(n)):
+   k=int(input(""))
+   A.append(k)
+print ("The array is ::>")
+for i in range(0, n):
+   print (A[i], end = ' ')
+rotate(A, n)
+print ("\nRotated array is")
+for i in range(0, n):
+   print (A[i])
 
     # OUTPUT:
+    # Enter size: 10
+    # Enter Elements : 0 1 2 3 4 5 6 7 8 9
     # Before Rotation: 0 1 2 3 4 5 6 7 8 9
     # After Rotation: 3 4 5 6 7 8 9 0 1 2
