82b73215a79a2d966fde53c7e0ab00cca0ffa9f4
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-20 21:52:59 +0530

    Update P03_GetMissingNumber.py

--- Arrays/P03_GetMissingNumber.py
+++ Arrays/P03_GetMissingNumber.py
@@ -1,22 +1,13 @@
 # Author: AKHILESH SANTOSHWAR
-from Arrays import Array
+def missing_numbers(num_list):
+      original_list = [x for x in range(num_list[0], num_list[-1] + 1)]
+      num_list = set(num_list)
+      return (list(num_list ^ set(original_list)))
+print("missing number")
+print(missing_numbers([1,2,3,4,6,7,10]))
 
-def findMissing(myArray, n):
-    n = n - 1
-    totalSum = (n * (n + 1)) // 2
-    for i in range(0, n):
-        totalSum -= myArray[i]
 
-    return totalSum
 
-if __name__ == '__main__':
-    myArray = Array(10)
-    for i in range(len(myArray)):
-        myArray.insert(i, i)
-    myArray.delete(4, 4)
-    print('Original Array:',myArray)
-    print('Missing Element:', findMissing(myArray, len(myArray)))
-
-    # OUTPUT:
-    # Original Array: 0 1 2 3 5 6 7 8 9 0
-    # Missing Element: 4
+# Output:
+#missing number
+#[5,8,9]
