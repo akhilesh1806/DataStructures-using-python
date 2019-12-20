208ab8accbd92862da3304833f0cca9ae50dfb3d
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-20 22:14:27 +0530

    Update P03_DecimalToBinary.py

--- Stack/P03_DecimalToBinary.py
+++ Stack/P03_DecimalToBinary.py
@@ -2,19 +2,34 @@
 
 # Decimal to binary using Stack
 
-import  Stack
+class Stack ():
 
-def dtob(decimal, base = 2):
-    myStack = Stack.Stack()
-    while decimal > 0:
-        myStack.push(decimal % base)
-        decimal //= base
+   def __init__(self):
+        self.items=[]
 
-    result = ''
-    while not myStack.isEmpty():
-        result += str(myStack.pop())
+    def push (self,item):
+        self.items.append(item)
 
-    return result
+    def pop (self):
+        return self.items.pop()
 
-if __name__ == '__main__':
-    print(dtob(15))
+    def size(self):
+        return len(self.items)
+
+s1= Stack()
+
+
+
+
+decNum= int(input("Enter the decimal num : "))
+
+newNum=decNum
+
+while newNum==1:
+
+    newNum= decNum%2
+    decNum = decNum//2
+    s1.push(newNum)
+    while s1.size() is not Null:
+        a=s1.pop()
+        print(a)
