ce8990b27405c3e2bad2fbfa383f32e57162a192
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-20 22:09:49 +0530

    Update P04_ReverseString.py

--- Stack/P04_ReverseString.py
+++ Stack/P04_ReverseString.py
@@ -2,18 +2,57 @@
 
 # Reverse a string using Stack
 
-import Stack
-
-def reverse(string):
-    myStack = Stack.Stack(len(string))
-    for i in string:
-        myStack.push(i)
-
-    result = ''
-    while not myStack.isEmpty():
-        result += myStack.pop()
-
-    return result
-
-if __name__ == '__main__':
-    print(reverse('omkar'))
+# Python program to reverse a string using stack 
+  
+# Function to create an empty stack.  
+# It initializes size of stack as 0 
+def createStack(): 
+    stack=[] 
+    return stack 
+  
+# Function to determine the size of the stack 
+def size(stack): 
+    return len(stack) 
+  
+# Stack is empty if the size is 0 
+def isEmpty(stack): 
+    if size(stack) == 0: 
+        return true 
+  
+# Function to add an item to stack .  
+# It increases size by 1  
+def push(stack,item): 
+    stack.append(item) 
+  
+#Function to remove an item from stack.  
+# It decreases size by 1 
+def pop(stack): 
+    if isEmpty(stack): return
+    return stack.pop() 
+  
+# A stack based function to reverse a string 
+def reverse(string): 
+    n = len(string) 
+      
+    # Create a empty stack 
+    stack = createStack() 
+  
+    # Push all characters of string to stack 
+    for i in range(0,n,1): 
+        push(stack,string[i]) 
+  
+    # Making the string empty since all  
+    #characters are saved in stack  
+    string="" 
+  
+    # Pop all characters of string and  
+    # put them back to string 
+    for i in range(0,n,1): 
+        string+=pop(stack) 
+          
+    return string 
+      
+# Driver program to test above functions 
+string="Akhilesh"
+string = reverse(string) 
+print("Reversed string is " + string) 
